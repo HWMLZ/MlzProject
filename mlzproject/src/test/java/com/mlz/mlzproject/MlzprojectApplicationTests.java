a513f29f90bb77c9e875c7945574fdb0d100fd87
fw\mlz <moliangzhao@greatge.com>
2023-02-15 14:51:33 +0800

    项目初始化

---
+++ mlzproject/src/test/java/com/mlz/mlzproject/MlzprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mlz.mlzproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlzprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
