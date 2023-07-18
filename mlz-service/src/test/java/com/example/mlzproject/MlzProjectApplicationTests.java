c9d493bc3731f2b240cde038911f5af16a1f27b0
moliangzhao <2692841607@qq.com>
2023-07-18 15:39:00 +0800

    项目结构搭建

---
+++ mlz-service/src/test/java/com/example/mlzproject/MlzProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mlzproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlzProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
